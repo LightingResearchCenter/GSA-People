6a372cf0a5f3a2b47d2665b14e147b919a1d9812
Geoffrey Jones <jonesg5@rpi.edu>
2014-07-21 17:42:24 -0400

    Function to enable dependencies

---
+++ initializedependencies.m
@@ -0,0 +1,19 @@
+function initializedependencies
+%INITIALIZEDEPENDENCIES Add necessary repos to working path
+%   Detailed explanation goes here
+
+% Find full path to github directory
+[githubDir,~,~] = fileparts(pwd);
+
+% Contruct repo paths
+cdfPath         = fullfile(githubDir,'LRC-CDFtoolkit');
+phasorPath      = fullfile(githubDir,'PhasorAnalysis');
+sleepPath       = fullfile(githubDir,'DaysimeterSleepAlgorithm');
+daysigramPath   = fullfile(githubDir,'DaysigramReport');
+lightHealthPath = fullfile(githubDir,'LHIReport');
+
+% Enable repos
+addpath(cdfPath,phasorPath,sleepPath,daysigramPath,lightHealthPath);
+
+end
+
