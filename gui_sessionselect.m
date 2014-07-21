21ca11fe482a639f16f52b72df6feb44ca606a68
Geoffrey Jones <jonesg5@rpi.edu>
2014-07-21 15:48:05 -0400

    GUI to select project and location

---
+++ gui_sessionselect.m
@@ -0,0 +1,14 @@
+function session = gui_sessionselect
+%GUI_SESSIONSELECT GUI to select desired session
+%   Detailed explanation goes here
+
+choice = menu('Choose a session',...
+    'Summer',...
+    'Winter');
+
+sessionArray = {'summer','winter'};
+
+session = sessionArray{choice};
+
+end
+
