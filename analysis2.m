928ce852e4a85b080c1d04c53221ebe183da98c2
Geoffrey Jones <jonesg5@rpi.edu>
2014-10-22 16:00:51 -0400

    Added analysis that excludes weekends

---
+++ analysis2.m
@@ -0,0 +1,265 @@
+function analysis2
+%ANALYSIS2
+
+% Enable dependecies
+initializedependencies;
+
+% Have user select project location and session
+[plainLocation,displayLocation] = gui_locationselect;
+[plainSession ,displaySession ] = gui_sessionselect ;
+
+% Construct project paths
+Paths = initializepaths(plainLocation,plainSession);
+[~,cdfPathArray] = searchdir(Paths.editedData,'cdf');
+[bedLogFileNameArray,bedLogPathArray] = searchdir(Paths.logs,'xlsx');
+
+% Extract subject from bed logs
+bedSubject = str2double(regexprep(bedLogFileNameArray,'.*(\d\d).*','$1'));
+
+% Preallocate and intialize resources
+[hFigure,~,~,units] = initializefigure1(1,'on');
+nFiles = numel(cdfPathArray);
+templateCell = cell(nFiles,1);
+
+Output = dataset;
+Output.subject = templateCell;
+%   sleep
+Output.nightsAveraged        = templateCell;
+Output.actualSleepTimeMins   = templateCell;
+Output.actualSleepPercent    = templateCell;
+Output.actualWakeTimeMins    = templateCell;
+Output.actualWakePercent     = templateCell;
+Output.sleepEfficiency       = templateCell;
+Output.sleepOnsetLatencyMins = templateCell;
+Output.sleepBouts            = templateCell;
+Output.wakeBouts             = templateCell;
+Output.meanSleepBoutTimeMins = templateCell;
+Output.meanWakeBoutTimeMins  = templateCell;
+%   phasor, IS, IV
+Output.phasorMagnitude       = templateCell;
+Output.phasorAngleHrs        = templateCell;
+Output.interdailyStability   = templateCell;
+Output.intradailyVariability = templateCell;
+%   averages
+Output.meanNonzeroCs            = templateCell;
+Output.logmeanNonzeroLux        = templateCell;
+Output.meanNonzeroActivity      = templateCell;
+Output.meanWorkdayCs            = templateCell;
+Output.logmeanWorkdayLux        = templateCell;
+Output.logmedianWorkdayLux      = templateCell;
+Output.meanWorkdayActivity      = templateCell;
+Output.meanPostWorkdayCs        = templateCell;
+Output.logmeanPostWorkdayLux    = templateCell;
+Output.logmedianPostWorkdayLux  = templateCell;
+Output.meanPostWorkdayActivity  = templateCell;
+
+
+for i1 = 1:nFiles
+    % Import data
+    Data = ProcessCDF(cdfPathArray{i1});
+    subject = Data.GlobalAttributes.subjectID{1};
+    logicalArray = logical(Data.Variables.logicalArray);
+    complianceArray = logical(Data.Variables.complianceArray(logicalArray));
+    bedArray = logical(Data.Variables.bedArray(logicalArray));
+    timeArray = Data.Variables.time(logicalArray);
+    activityArray = Data.Variables.activity(logicalArray);
+    csArray = Data.Variables.CS(logicalArray);
+    illuminanceArray = Data.Variables.illuminance(logicalArray);
+    
+    % Set subject
+    Output.subject{i1,1} = subject;
+    
+    % Check useable data
+    if numel(timeArray(complianceArray)) < 24
+        continue
+    end
+    
+    % Match and import bed log
+    bedIdx = bedSubject == str2double(subject);
+    [bedTimeArray,riseTimeArray] = importbedlog(bedLogPathArray{bedIdx});
+    
+    % Daysigram
+    sheetTitle = ['GSA - ',displayLocation,' - ',displaySession,' - Subject ',subject];
+    daysigramFileID = ['subject',subject];
+    generatedaysigram(sheetTitle,timeArray(complianceArray),...
+        activityArray(complianceArray),csArray(complianceArray),...
+        'cs',[0,1],8,Paths.plots,daysigramFileID)
+    
+    % Light and Health Report/ Phasor Analysis
+    figTitle = ['GSA - ',displayLocation,' - ',displaySession];
+    Phasor = phasorprep(subject,figTitle,hFigure,units,Paths,...
+        complianceArray,bedArray,timeArray,csArray,activityArray,...
+        illuminanceArray);
+    
+    % Averages
+    [Average,WorkAverage,PostWorkAverage] = prepaverages(...
+        timeArray,csArray,activityArray,illuminanceArray,...
+        complianceArray,bedArray,bedTimeArray);
+    
+    % Sleep Analysis
+    [Sleep,nIntervalsAveraged] = sleepprep(timeArray,activityArray,...
+        bedTimeArray,riseTimeArray,complianceArray);
+    
+    % Assign output
+    %   sleep
+    Output.nightsAveraged{i1,1}         = nIntervalsAveraged;
+    Output.actualSleepTimeMins{i1,1}    = Sleep.actualSleepTime;
+    Output.actualSleepPercent{i1,1}     = Sleep.actualSleepPercent;
+    Output.actualWakeTimeMins{i1,1}     = Sleep.actualWakeTime;
+    Output.actualWakePercent{i1,1}      = Sleep.actualWakePercent;
+    Output.sleepEfficiency{i1,1}        = Sleep.sleepEfficiency;
+    Output.sleepOnsetLatencyMins{i1,1}  = Sleep.sleepLatency;
+    Output.sleepBouts{i1,1}             = Sleep.sleepBouts;
+    Output.wakeBouts{i1,1}              = Sleep.wakeBouts;
+    Output.meanSleepBoutTimeMins{i1,1}  = Sleep.meanSleepBoutTime;
+    Output.meanWakeBoutTimeMins{i1,1}   = Sleep.meanWakeBoutTime;
+
+    %   phasor, IS, IV
+    Output.phasorMagnitude{i1,1}        = Phasor.phasorMagnitude;
+    Output.phasorAngleHrs{i1,1}         = Phasor.phasorAngleHrs;
+    Output.interdailyStability{i1,1}    = Phasor.interdailyStability;
+    Output.intradailyVariability{i1,1}  = Phasor.intradailyVariability;
+
+    %   averages
+    Output.meanNonzeroCs{i1,1}          = Average.cs;
+    Output.logmeanNonzeroLux{i1,1}      = Average.illuminance;
+    Output.meanNonzeroActivity{i1,1}    = Average.activity;
+    Output.meanWorkdayCs{i1,1}          = WorkAverage.cs;
+    Output.logmeanWorkdayLux{i1,1}      = WorkAverage.illuminance;
+    Output.logmedianWorkdayLux{i1,1}    = WorkAverage.medianIlluminance;
+    Output.meanWorkdayActivity{i1,1}    = WorkAverage.activity;
+    Output.meanPostWorkdayCs{i1,1}      = PostWorkAverage.cs;
+    Output.logmeanPostWorkdayLux{i1,1}  = PostWorkAverage.illuminance;
+    Output.logmedianPostWorkdayLux{i1,1} = PostWorkAverage.medianIlluminance;
+    Output.meanPostWorkdayActivity{i1,1}= PostWorkAverage.activity;
+end
+
+close all
+
+outputCell = dataset2cell(Output);
+varNameArray = outputCell(1,:);
+prettyVarNameArray = lower(regexprep(varNameArray,'([^A-Z])([A-Z0-9])','$1 $2'));
+outputCell(1,:) = prettyVarNameArray;
+
+runtime = datestr(now,'yyyy-mm-dd_HHMM');
+resultsPath = fullfile(Paths.results,['results_',runtime,'_GSA_',plainLocation,'_',plainSession,'_sans-weekends','.xlsx']);
+xlswrite(resultsPath,outputCell);
+
+end
+
+
+function Output = phasorprep(subject,figTitle,hFigure,units,Paths,complianceArray,bedArray,timeArray,csArray,activityArray,illuminanceArray)
+clf;
+
+wkendIdx = createweekend(timeArray);
+
+% replace in bed time
+csArray(bedArray) = 0;
+activityArray(bedArray) = 0;
+illuminanceArray(bedArray) = 0;
+
+% remove only large noncompliance while awake
+complianceArray = adjustcrop(timeArray,complianceArray,bedArray);
+timeArray(~complianceArray | wkendIdx) = [];
+csArray(~complianceArray | wkendIdx) = [];
+activityArray(~complianceArray | wkendIdx) = [];
+illuminanceArray(~complianceArray | wkendIdx) = [];
+
+Output = generatereport(Paths.plots,timeArray,csArray,activityArray,...
+    illuminanceArray,[subject,' sans-weekends'],hFigure,units,figTitle);
+end
+
+
+function wkendIdx = createweekend(timeArray)
+
+dayArray        = floor(timeArray);
+dayOfWeekArray	= weekday(dayArray); % Sunday = 1, Monday = 2, etc.
+wkendIdx        = dayOfWeekArray == 1 | dayOfWeekArray == 7;
+
+end
+
+function [workIdx,postWorkIdx] = createworkday(timeArray,bedTimeArray)
+
+workStart = 8/24;
+workEnd   = 17/24;
+
+dayArray       = unique(floor(timeArray));
+dayOfWeekArray = weekday(dayArray); % Sunday = 1, Monday = 2, etc.
+workDaysIdx    = dayOfWeekArray >= 2 & dayOfWeekArray <= 6;
+workDayArray   = dayArray(workDaysIdx);
+
+workStartArray = workDayArray + workStart;
+workEndArray   = workDayArray + workEnd;
+
+workIdx = false(size(timeArray));
+postWorkIdx = false(size(timeArray));
+for j1 = 1:numel(workStartArray)
+    tempWorkIdx = timeArray > workStartArray(j1) & timeArray <= workEndArray(j1);
+    workIdx = workIdx | tempWorkIdx;
+
+    diffBedTime = bedTimeArray - workEndArray(j1);
+    currentBedTime = bedTimeArray(diffBedTime<1 & diffBedTime>0);
+    if numel(currentBedTime) == 1
+        tempPostWorkIdx = timeArray > workEndArray(j1) & timeArray <=currentBedTime;
+        postWorkIdx = postWorkIdx | tempPostWorkIdx;
+    end
+end
+
+end
+
+
+function [Average,WorkAverage,PostWorkAverage] = prepaverages(timeArray,csArray,activityArray,illuminanceArray,complianceArray,bedArray,bedTimeArray)
+
+validIdx = complianceArray & ~bedArray;
+
+timeArray(~validIdx) = [];
+csArray(~validIdx) = [];
+activityArray(~validIdx) = [];
+illuminanceArray(~validIdx) = [];
+
+[workIdx,postWorkIdx] = createworkday(timeArray,bedTimeArray);
+
+Average = daysimeteraverages(csArray,illuminanceArray,activityArray);
+WorkAverage = daysimeterworkaverages(csArray(workIdx),...
+    illuminanceArray(workIdx),activityArray(workIdx));
+PostWorkAverage = daysimeterworkaverages(csArray(postWorkIdx),...
+    illuminanceArray(postWorkIdx),activityArray(postWorkIdx));
+
+end
+
+
+function [Sleep,nIntervalsAveraged] = sleepprep(timeArray,activityArray,bedTimeArray,riseTimeArray,complianceArray)
+
+timeArray(~complianceArray) = [];
+activityArray(~complianceArray) = [];
+
+analysisStartTimeArray = bedTimeArray  - 20/(60*24);
+analysisEndTimeArray   = riseTimeArray + 20/(60*24);
+
+startDayArray = floor(analysisStartTimeArray);
+startDayOfWeekArray = weekday(startDayArray); % Sunday = 1, Monday = 2, etc.
+wkendIdx = startDayOfWeekArray == 1 | startDayOfWeekArray == 7;
+
+bedTimeArray(wkendIdx) = [];
+riseTimeArray(wkendIdx) = [];
+analysisStartTimeArray(wkendIdx) = [];
+analysisEndTimeArray(wkendIdx) = [];
+
+nIntervals = numel(bedTimeArray);
+dailySleep = cell(nIntervals,1);
+    
+for i1 = 1:nIntervals
+    % Perform analysis
+    try
+        dailySleep{i1} = sleepAnalysis(timeArray,activityArray,...
+        analysisStartTimeArray(i1),analysisEndTimeArray(i1),...
+        bedTimeArray(i1),riseTimeArray(i1),'auto');
+    catch err
+        continue
+    end
+end
+
+% Average results
+[Sleep,nIntervalsAveraged] = averageanalysis(dailySleep);
+
+end
